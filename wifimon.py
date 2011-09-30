23a4614800f17fa91ed64da667e18e57300d42f4
Noah S. Bieler <noah.bieler@gmx.ch>
2011-09-30 16:44:58 +0200

    First "working" version :)

--- wifimon.py
+++ wifimon.py
@@ -9,11 +9,11 @@ wifimon monitors your wifi connection and sits in your systray
 """
 
 # Imports
-#import os, sys
 import re       # regular expression
 import time     # for sleep()
 import signal   # To catch sig int (ctrl-c)
 import sys      # To catch sig int (ctrl-c)
+import os       # For creating nice file paths
 
 from PyQt4 import QtGui, QtCore
 
@@ -22,100 +22,110 @@ class Conf:
     def __init__(self):
         self.interface = "no interface assigned!"
         self.percentage = -1
-        self.intervall = 10
-        #self.statfile = "/proc/net/wireless"
-        self.statfile = "test_wlan"
+        self.interval = 1
+        self.statfile = "/proc/net/wireless"
+        #self.statfile = "test_wlan"
         #self.statfile = "test_nowlan"
         self.iconPath = "./img/"
 
-# Read in the configuration file
-def read_conf():
-    return 0
-    
-# The icon in the systray
-class WifiIcon(QtGui.QWidget):
-    start = QtCore.pyqtSignal()
-    show_tray = QtCore.pyqtSignal()
-    def __init__(self):
-        QtGui.QWidget.__init__(self)
-        self.createActions()
-        self.createTrayIcon()
-        self.trayIcon.show()
+# Monitors the state of the wireless interface
+class WifiMonitor(QtCore.QObject):
+    signal_display_status = QtCore.pyqtSignal(int, str)
+    def __init__(self, parent = None):
+        QtCore.QObject.__init__(self, parent)
+        self.timer = QtCore.QTimer(self)
+        self.timer.setInterval(conf.interval * 1000)
+        self.timer.timeout.connect(self.update_status)
+        self.start()
+        print("Constructed WifiMonitor")
 
-    def createActions(self):
+    def start(self):
+        self.timer.start()
 
-        self.quitAction = QtGui.QAction(self.tr("&Quit"), self)
-        QtCore.QObject.connect(self.quitAction, QtCore.SIGNAL("triggered()"),
-                    QtGui.qApp, QtCore.SLOT("quit()"))
-        self.start.connect(self.run)
-
-    def createTrayIcon(self):
-        self.trayIconMenu = QtGui.QMenu(self)
-        self.trayIconMenu.addAction(self.quitAction)
-
-        self.trayIcon = QtGui.QSystemTrayIcon(self)
-        self.trayIcon.setContextMenu(self.trayIconMenu)
-        #pathToIcon = "/usr/share/icons/default.kde4/22x22/devices/network-wireless-connected-100.png"
-        pathToIcon = conf.iconPath + "wifi_50.svg"
-        self.trayIcon.setIcon(QtGui.QIcon(pathToIcon))
-        self.show_tray.connect(self.trayIcon.show)
+    def stop(self):
+        self.timer.stop()
 
     # Read in the satus of the connection
-    def get_status(self):
-    
+    def update_status(self):
+        print("I will update the status")
         # open status file and discard first two lines
-        f = open(conf.statfile)
-        f.readline()
-        f.readline()
+        with open(conf.statfile) as f:
+            f.readline()
+            f.readline()
+            line = f.readline()
+
+        if line:
+            print(line)
+            m = re.search("(\S+\d):[ ]+(\d+)[ ]+(\d+)\.", line)
+            interface = m.group(1)
+            percentage = int(m.group(3))
     
-        l = f.readline()
-        if (len(l) != 0):
-            print(l)
-            m = re.search("(\S+\d):[ ]+(\d+)[ ]+(\d+)\.", l)
-            conf.interface = m.group(1)
-            #status = m.group(2)
-            conf.percentage = int(m.group(3))
-    
-        return 0
-    
-    # Display the status
-    def display_status(self):
-    
-        if (conf.percentage >= 0):
-            print("%s @ %d" % (conf.interface, conf.percentage))
-            filePercentage = (int(conf.percentage / 25) + 1) * 25
-            pathToIcon = conf.iconPath + "wifi_" + str(filePercentage) + ".svg"
-            self.trayIcon.setIcon(QtGui.QIcon(pathToIcon))
+        f.close()
+        self.signal_display_status.emit(percentage, interface)
+        
+# The system tray icon
+class WifiStatusIcon(QtGui.QSystemTrayIcon):
+    def __init__(self, parent = None):
+        QtGui.QSystemTrayIcon.__init__(self, parent)
+        self.createActions()
+        self.createTrayIcon()
+        print("Created Status Icon")
+
+    def createActions(self):
+        self.quitAction = QtGui.QAction(self.tr("&Quit"), self)
+        self.quitAction.triggered.connect(QtGui.qApp.quit)
+
+    def createTrayIcon(self):
+        self.trayIconMenu = QtGui.QMenu()
+        self.trayIconMenu.addAction(self.quitAction)
+        self.setContextMenu(self.trayIconMenu)
+        pathToIcon = conf.iconPath + "wifi_none.svg"
+        self.setIcon(QtGui.QIcon(pathToIcon))
+
+    def show_wifi_status(self, percentage, interface):
+        if percentage >= 0:
+            print("%s @ %d" % (interface, percentage))
+            if percentage:
+                filePercentage = (int((percentage - 1) / 25) + 1) * 25
+            else:
+                filePercentage = 0
+            pathToIcon = os.path.join(conf.iconPath, 
+                                      "wifi_{0}.svg".format(filePercentage))
         else:
             print("No wifi connection detected!")
-            pathToIcon = conf.iconPath + "wifi_none.svg"
-            self.trayIcon.setIcon(QtGui.QIcon(pathToIcon))
+            pathToIcon = os.path.join(conf.iconPath, "wifi_none.svg")
     
+        self.setIcon(QtGui.QIcon(pathToIcon))
+        self.show()
         print(pathToIcon)
-        return 0
 
-    # main loop
-    def run(self):
-        while True:
-            self.get_status()
-            self.display_status()
-            time.sleep(conf.intervall)
 
-# What to do, in the case of interruption
-def signal_handler(signal, frame):
-    print("\nProgram was interrupted!")
-    sys.exit(0)
+# Displays the state of the wireless connection
+class WifiStatusWidget(QtGui.QWidget):
+    def __init__(self, parent = None):
+        QtGui.QWidget.__init__(self, parent)
+        print("Constructed WifiStatusWidget")
 
+    # Display the status
+    def show_wifi_status(self, percentage, interface):
+        if percentage >= 0:
+            print("widget: %s @ %d" % (interface, percentage))
+        else:
+            print("No wifi connection detected!")
+
+class WifiMonitorApplication(QtGui.QApplication):
+    def __init__(self, args):
+        QtGui.QApplication.__init__(self, args)
+        self.wifi_monitor = WifiMonitor()
+        self.status_widget = WifiStatusWidget()
+        self.status_icon = WifiStatusIcon()
+        for widget in (self.status_widget, self.status_icon):
+            self.wifi_monitor.signal_display_status.connect(widget.show_wifi_status)
+            widget.show()
 
 if (__name__ == "__main__"):
-    signal.signal(signal.SIGINT, signal_handler)
     conf = Conf()
-    read_conf()
     
-    app = QtGui.QApplication(sys.argv)
-    icon = WifiIcon()
-    icon.start.emit()
-    icon.show_tray.emit()
+    app = WifiMonitorApplication(sys.argv)
+    print("Start GUI")
     app.exec_()
-    sys.exit()
-
